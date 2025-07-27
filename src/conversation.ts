54eb15695a2456b38753c7212531b5f338622582
daijinru <jeocat@163.com>
2025-07-27 13:09:54 +0800

    stream handle -> keyword

--- src/conversation.ts
+++ src/conversation.ts
@@ -7,41 +7,44 @@ const getKeywordClassification = (callback, loadingCallback?) => {
   let interval = setInterval(() => {
     loadingCallback('.')
   }, 1000)
-
-  fetchEventSource('http://localhost:8080/task', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      id: generateMsgId(),
-      text: '[keyword_classification]',
-    }),
-    onopen: (res) => {
-      console.log('open', res)
-      if (res.ok) return Promise.resolve()
-    },
-    onmessage: (line) => {
-      if (interval) {
-        clearInterval(interval)
-        interval = null
-      }
-      try {
-        const parsed = JSON.parse(line.data)
-        console.log('<intent parsed>', parsed)
-        const payload = parsed.payload
-        if (payload.type !== 'text') return
-        callback(payload.payload.content)
-      } catch (error) {
-        console.error(error)
-      }
-    },
-    onclose: () => {
-    },
-    onerror: (err) => {
-      console.log('error', err)
-    },
-  })
+  setTimeout(() => {
+    fetchEventSource('http://localhost:8080/task', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: generateMsgId(),
+        text: '[keyword_classification]',
+      }),
+      onopen: (res) => {
+        console.log('open', res)
+        if (res.ok) return Promise.resolve()
+      },
+      onmessage: (line) => {
+        if (interval) {
+          clearInterval(interval)
+          interval = null
+        }
+        try {
+          if (line.event !== 'text') return
+          const data = JSON.parse(line.data)
+          console.log('<intent parsed>', data)
+          if (data.type !== 'text') return
+          const payload = data.payload
+          if (payload.type !== 'text') return
+          callback(payload.content)
+        } catch (error) {
+          console.error(error)
+        }
+      },
+      onclose: () => {
+      },
+      onerror: (err) => {
+        console.log('error', err)
+      },
+    })
+  }, 1500)
 }
 
 export { getKeywordClassification }
